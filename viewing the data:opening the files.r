b4ad33c81c1c4b625394fc7d30f085c8af6fcf30
Mae Lalou <mae.lalou25@imperial.ac.uk>
2025-11-30 16:35:10 +0000

    simply opened the files to view data

---
+++ viewing the data:opening the files.r
@@ -0,0 +1,12 @@
+library(tidyverse)
+
+#setwd("/Users/maelalou/Desktop/Data Science/C10 IDS project/data sets")
+
+
+continents <- read.csv("continents-according-to-our-world-in-data.csv")
+gdpc <- read.csv("gdp-per-capita-worldbank.csv")
+youth <- read.csv("youth-not-in-education-employment-training.csv")
+
+View(continents)
+View(gdpc)
+View(youth)
