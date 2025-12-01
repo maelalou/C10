acd0a7e97003ff25e9df562a711b80dcc1db7052
Rinta Seto <rintaseto@Rintas-MacBook-Air.local>
2025-12-01 15:11:38 +0000

    Rinta: Testing github... Delete later

---
+++ Main.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+
+# For Rinta to download csv files...
+# If you are downloading csvs, please make sure that the name of the dataset is the same!
+continents <- read.csv("/Users/rintaseto/Downloads/EFDS/Intro to Data Science/Mid term/data sets/continents-according-to-our-world-in-data.csv")
+gdp <- read.csv("/Users/rintaseto/Downloads/EFDS/Intro to Data Science/Mid term/data sets/gdp-per-capita-worldbank.csv")
+youth <- read.csv("/Users/rintaseto/Downloads/EFDS/Intro to Data Science/Mid term/data sets/youth-not-in-education-employment-training.csv")
