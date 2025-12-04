9b007ff5303abcfb3078f10d779ae990d1b2739c
Rinta Seto <rintaseto@Rintas-MacBook-Air.local>
2025-12-04 14:07:48 +0000

    calculated sd for continets including all countries all ldcs all non-ldcs

--- calculate sd data.R
+++ calculate sd data.R
@@ -28,7 +28,9 @@ sd_5yr_LDCs <- continent_ldc_growth %>%
   group_by(continent) %>%
   mutate(sd = cal_5yr_sd(cont_avg_gdp_growth))
 
-#sd_5yr_nonLDCs
+sd_5yr_nonLDCs <- continent_noldc_growth %>%
+  group_by(continent) %>%
+  mutate(sd = cal_5yr_sd(cont_avg_gdp_growth))
 
 
 # Create output directory
@@ -40,4 +42,4 @@ write_csv(sd_5yr_all, file.path(output_dir, "sd_5yr_all.csv"))
 
 write_csv(sd_5yr_LDCs, file.path(output_dir, "sd_5yr_LDCs.csv"))
 
-
+write_csv(sd_5yr_nonLDCs, file.path(output_dir, "sd_5yr_nonLDCs.csv"))
