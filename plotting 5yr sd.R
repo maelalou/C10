da4f3e929378df41260669220ef05c67a4a07308
Rinta Seto <rintaseto@Rintas-MacBook-Air.local>
2025-12-08 10:44:10 +0000

    added titles to 5yr SDs

--- plotting 5yr sd.R
+++ plotting 5yr sd.R
@@ -4,16 +4,22 @@ library(dplyr)
 library(tidyr)
 library(ggpubr)
 
-p_sd_5yr_all <- ggplot(data = sd_5yr_all, aes(year, sd, col = continent)) + 
+p_sd_5yr_all <- ggplot(data = na.omit(sd_5yr_all), aes(year, sd, col = continent)) + 
   geom_point() + 
-  geom_line()
+  geom_line() + 
+  labs(title = "SD of 5 rolling years (World)", subtitle = "For Continents of Africa, Asia, Europe, North America, South America and Oceania", x = "Year", y = "Average SD", caption = "Source: World Bank")  +
+  scale_color_discrete(labels = toupper, name = "Continent") 
 
-p_sd_5yr_LDCs <- ggplot(data = sd_5yr_ldc, aes(year, sd, col = continent)) + 
+p_sd_5yr_LDCs <- ggplot(data = na.omit(sd_5yr_ldc), aes(year, sd, col = continent)) + 
   geom_point() + 
-  geom_line()
+  geom_line() + 
+  labs(title = "SD of 5 rolling years (LDCs)", subtitle = "For Continents of Africa, Asia, North America, and Oceania", x = "Year", y = "Average SD", caption = "Source: World Bank")  +
+  scale_color_discrete(labels = toupper, name = "Continent") 
 
-p_sd_5yr_nonLDCs <- ggplot(data = sd_5yr_noldc, aes(year, sd, col = continent)) + 
+p_sd_5yr_nonLDCs <- ggplot(data = na.omit(sd_5yr_noldc), aes(year, sd, col = continent)) + 
   geom_point() + 
-  geom_line()
+  geom_line() + 
+  labs(title = "SD of 5 rolling years (Non-LDCs)", subtitle = "For Continents of Africa, Asia, Europe, North America, South America and Oceania", x = "Year", y = "Average SD", caption = "Source: World Bank")  +
+  scale_color_discrete(labels = toupper, name = "Continent") 
 
 
