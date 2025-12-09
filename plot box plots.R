67c9230727027a158a6e75f3f48357ca1765489a
Rinta Seto <rintaseto@Rintas-MacBook-Air.local>
2025-12-09 13:45:06 +0000

    Remove .DS_Store and add to gitignore

--- plot box plots.R
+++ plot box plots.R
@@ -31,7 +31,7 @@ p_box_ldc <- ggarrange(p_box_ldc_pre, p_box_ldc_post, ncol=1, nrow = 2)
 
 p_box_noldc_pre <- ggplot(data = filter(continent_noldc_growth, year <= 2014), aes(continent, cont_avg_gdp_growth)) + 
   geom_boxplot() + 
-  labs(title = "Box Plot for Continents Including All Countries", subtitle = "Pre-2015", x = "", y = "Average GDP per Capita Growth (%)")
+  labs(title = "Box Plot for Continents Including Non-LDCs", subtitle = "Pre-2015", x = "", y = "Average GDP per Capita Growth (%)")
 
 p_box_noldc_post <- ggplot(data = filter(continent_noldc_growth, year >= 2015), aes(continent, cont_avg_gdp_growth)) + 
   geom_boxplot() + 
